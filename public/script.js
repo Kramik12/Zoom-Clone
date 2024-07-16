352d763881b56bda6ef75296f8db9becdb8fff91
Kramik <kramikkafle7@gmail.com>
2024-07-16 15:59:09 +0545

    first commit

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
-    port: '3001'
+    port: '3002'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true;
@@ -12,11 +12,30 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
 })
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
 
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId)
