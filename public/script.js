8a133486275e5e8de32a4cec576cb7c50b1e6b12
Kramik <kramikkafle7@gmail.com>
2024-07-17 16:40:26 +0545

    first commit

--- public/script.js
+++ public/script.js
@@ -15,11 +15,19 @@ navigator.mediaDevices.getUserMedia({
 
     myPeer.on('call', call => {
         call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
     })
 
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream)
     })
+})
+
+socket.on('user-disconnected', userId => {
+    console.log(userId)
 })
 
 myPeer.on('open', id => {
